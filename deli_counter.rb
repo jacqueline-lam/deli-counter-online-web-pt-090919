836d75d916292488ae01b5231662078cd3b332ff
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-18 03:17:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def line(deli)
   if deli.length == 0
     puts "The line is currently empty."
@@ -8,6 +10,7 @@ def line(deli)
     end
     
     puts line_string 
+    binding.pry
   end
 end
 
