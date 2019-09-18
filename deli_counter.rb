75ca157d1771cefd6c8484ec98cdbfd62e78f1bb
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-18 03:18:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,17 +9,16 @@ def line(deli)
       line_string += " #{position + 1}. #{customer}"
     end
     
-    puts line_string 
-    binding.pry
+    puts line_string #=> 
   end
 end
 
-# def take_a_number(deli, new_customer)
-#   puts "Welcome #{new_customer}. You are number #{(katz_deli.length + 1).to_s} in line."
-# end
+def take_a_number(deli, new_customer)
+  puts "Welcome #{new_customer}. You are number #{(katz_deli.length + 1).to_s} in line."
+end
 
-# def now_serving()
-#   puts "Currently serving #{katz_deli[0]}"
-#   deli.pop
-#   puts "There is nobody waiting to be served!" if katz_deli.length == 0
-# end
+def now_serving()
+  puts "Currently serving #{katz_deli[0]}"
+  deli.pop
+  puts "There is nobody waiting to be served!" if katz_deli.length == 0
+end
