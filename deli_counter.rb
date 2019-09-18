e62fad4b1acd698594cef4762338a40a873a03fa
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-18 03:16:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,11 @@ def line(deli)
   if deli.length == 0
     puts "The line is currently empty."
   elsif deli.length >= 1
-    deli.each_with_index do |customer, position|
       line_string = "The line is currently:"
+    deli.each_with_index do |customer, position|
       line_string += " #{position + 1}. #{customer}"
     end
+    
     puts line_string 
   end
 end
